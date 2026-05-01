3720af1b4c256047061c3f6c9f2bffed3bf7a162
TranCongTuan <trancongtuan2020xt@gmail.com>
2026-05-01 08:03:19 +0700

    Test trong

---
+++ xinchao/test1.py
@@ -0,0 +1,4 @@
+print("Xin chào, đây là một đoạn mã Python đơn giảnkihcnios!")
+print("Chúc bạn một ngày tốt lành!")
+print("Xin cha Tran Cong Tuan day la tesst tu phia git check den local neu co thu la roi ")
+print("Xin chao`, day la test tu phia git check den local neu co thula roi")
