68387d1404d09bb660785691f4cf6d6b8b70a419
TranCongTuan <trancongtuan2020xt@gmail.com>
2026-05-01 08:45:42 +0700

    Tesst cai kia

--- index.py
+++ index.py
@@ -3,3 +3,4 @@ print("Xin chào, đây là một đoạn mã Python đơn giản!")
 # Ví dụ: in ra một thông điệp khác
 print("Chúc bạn một ngày tốt lành!")
 print("Xin cha Tran Cong Tuan day la tesst tu phia git check den local neu co thu la roi ")
+print("Xin chao test nhe")
