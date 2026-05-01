0aa10054e7c81928deac6b30588de22caa09368d
Trần Công Tuấn <trancongtuan2020xt@gmail.com>
2026-05-01 07:57:19 +0700

    Add test print statement to index.py
    chac dung

--- index.py
+++ index.py
@@ -2,3 +2,4 @@ print("Xin chào, đây là một đoạn mã Python đơn giản!")
 # Bạn có thể thêm bất kỳ mã nào bạn muốn ở đây
 # Ví dụ: in ra một thông điệp khác
 print("Chúc bạn một ngày tốt lành!")
+print("Xin cha Tran Cong Tuan day la tesst tu phia git check den local neu co thu la roi ")
